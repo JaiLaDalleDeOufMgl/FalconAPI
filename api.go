1a36c8bf60c72b49b2e4dfccc7088eb0fe6f51c6
Bruno DelPero <54162106+Shazuub@users.noreply.github.com>
2022-11-20 15:41:01 +0100

    Add files via upload

--- api.go
+++ api.go
@@ -7,10 +7,13 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"time"
 )
 
 type Tag struct {
-	EXPIRES string `json:"expires"`
+	DAY   string `json:"day"`
+	MONTH string `json:"month"`
+	YEAR  string `json:"year"`
 }
 
 func Check(w http.ResponseWriter, r *http.Request) {
@@ -24,17 +27,25 @@ func License(w http.ResponseWriter, r *http.Request) {
 	}
 	defer db.Close()
 	ip, _, err := net.SplitHostPort(r.RemoteAddr)
-	results, err := db.Query("SELECT expires FROM license WHERE license_key = ? AND ip = ?", r.URL.Path[9:], ip)
+	results, err := db.Query("SELECT day, month, year FROM license WHERE license_key = ? AND ip = ?", r.URL.Path[9:], ip)
 	if err != nil {
 		panic(err.Error())
 	}
 	for results.Next() {
 		var tag Tag
-		err = results.Scan(&tag.EXPIRES)
+		err = results.Scan(&tag.DAY, &tag.MONTH, &tag.YEAR)
 		if err != nil {
 			panic(err.Error())
 		}
-		fmt.Fprintf(w, `{status: "success", expires: "%s"}`, tag.EXPIRES)
+		dt := time.Now()
+		if tag.YEAR >= dt.Format("2006") {
+			if tag.MONTH >= dt.Format("01") {
+				if tag.DAY >= dt.Format("02") {
+					fmt.Println("license ok")
+					fmt.Fprintf(w, `{status: "success"}`)
+				}
+			}
+		}
 	}
 }
 
